4dc126481ff85acde6bcbc2ee0c5dce313a8c91d
Niklas D <niklas.dobberstein@gmx.de>
2024-10-18 18:30:02 +0200

    Started with tensor

--- src/numtorch/autograd/tensor.py
+++ src/numtorch/autograd/tensor.py
@@ -20,8 +20,63 @@ def convert_array_to_value_arr(arr):
 
 def broadcast_to(t1: Tensor, t2: Tensor):
     """
-    Broadcasts t2 to the shape of t1
+    Broadcasts t2 to the shape of t1 if possible.
+    Raises a ValueError if broadcasting is not possible.
     """
+    shape1 = t1.shape
+    shape2 = t2.shape
+
+    # Check if broadcasting is possible
+    if len(shape2) > len(shape1):
+        raise ValueError(f"Cannot broadcast: shape of t2 {shape2} is larger than shape of t1 {shape1}")
+
+    # Add leading dimensions to shape2 if necessary
+    shape2 = (1,) * (len(shape1) - len(shape2)) + shape2
+
+    # Verify that broadcasting is possible for each dimension
+    for dim1, dim2 in zip(shape1, shape2):
+        if dim1 != dim2 and dim2 != 1:
+            raise ValueError(f"Cannot broadcast: dimension mismatch between t1 {shape1} and t2 {shape2}")
+
+    # Create a new Tensor with the broadcasted values
+    def expand(data, target_shape):
+        """
+        Recursively expand the dimensions of data to match the target shape.
+        """
+        if isinstance(data, list):
+            # If we're already at the correct shape, return data
+            if len(data) == target_shape[0]:
+                return [expand(d, target_shape[1:]) for d in data]
+            # If the current dimension is 1, broadcast it by repeating the data
+            elif len(data) == 1:
+                return [expand(data[0], target_shape[1:]) for _ in range(target_shape[0])]
+            else:
+                raise ValueError("Data cannot be expanded to the target shape")
+        else:
+            # This is a Value object; no further expansion needed
+            return data
+
+    expanded_data = expand(t2._data, shape1)
+    out = Tensor(expanded_data, children=(t2,), op="broadcast")
+
+    def _backward():
+        """
+        Backward function for broadcasting. The gradient is summed along the dimensions where t2 was broadcasted.
+        """
+        grad = out.grad
+
+        reduced_grad = grad
+        for i, (dim1, dim2) in enumerate(zip(shape1, shape2)):
+            if dim2 == 1:
+                reduced_grad = np.sum(reduced_grad, axis=i, keepdims=True)
+
+        reduced_grad = np.squeeze(reduced_grad)
+
+        t2.grad += reduced_grad
+
+    out._backward = _backward
+
+    return out
 
 
 class Tensor:
@@ -44,6 +99,9 @@ class Tensor:
                 return data.shape
 
         return get_shape(self._data)
+
+    def __len__(self):
+        return len(self._data)
 
     def __add__(self, other):
         other = other if isinstance(other, Tensor) else Tensor(other)
